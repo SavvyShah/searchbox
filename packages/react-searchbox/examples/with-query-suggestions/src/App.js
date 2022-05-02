90e0f7386b393101e7c34199fc0222d80632a2e7
Shubham Shah <shubhamshahrising@gmail.com>
2022-05-02 14:41:23 +0530

    Base setup

--- packages/react-searchbox/examples/with-query-suggestions/src/App.js
+++ packages/react-searchbox/examples/with-query-suggestions/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { SearchBox, SearchComponent } from '@appbaseio/react-searchbox';
-import ReactPaginate from 'react-paginate';
 
 import './styles.css';
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <SearchBox id="search" enablePopularSuggestions autosuggest={false} />
+      <SearchComponent
+        id="results"
+        react={{ and: ['search'] }}
+        aggregationField="genres_data.keyword"
+      >
+        {({ results, loading, size, setValue, setFrom }) => {
+          console.log({ results, loading });
+          return <div></div>;
+        }}
+      </SearchComponent>
+    </div>
+  );
 };
 
 export default App;
