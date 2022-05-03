950ed2870fc2a0ac27fa94222907f2eb12d0077b
Shubham Shah <shubhamshahrising@gmail.com>
2022-05-03 11:30:25 +0530

    No results view

--- packages/react-searchbox/examples/with-query-suggestions/src/App.js
+++ packages/react-searchbox/examples/with-query-suggestions/src/App.js
@@ -1,32 +1,50 @@
 import React from 'react';
-import { SearchBox, SearchComponent } from '@appbaseio/react-searchbox';
+import { SearchBox } from '@appbaseio/react-searchbox';
 
 import './styles.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="page">
+      <h2>
+        React Searchbox with query suggestions{' '}
+        <span style={{ fontSize: '1rem' }}>
+          <a
+            href="https://docs.appbase.io/docs/reactivesearch/react-searchbox/apireference/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            API reference
+          </a>
+        </span>
+      </h2>
       <SearchBox
         id="search"
         aggregationField="genres_data.keyword"
         size={5}
         aggregationSize={5}
         enablePopularSuggestions
+        popularSuggestionsConfig={{ index: 'movies-store-app' }}
         render={({ data, value, loading, rawData }) => {
           const key = 'genres_data.keyword';
           if (!value || loading) return null;
+          const popularResults = data.filter(
+            res => res._suggestion_type === 'popular'
+          );
+          const indexResults = data.filter(
+            res => res._suggestion_type === 'index'
+          );
+          if (!popularResults.length && !indexResults.length) return null;
           return (
             <div className="result">
               <div className="resultSuggestion list">
                 <div className="listHead">Suggestions</div>
                 <div className="listBody">
-                  {data
-                    .filter(res => res._suggestion_type === 'index')
-                    .map(res => (
-                      <div className="suggestion">
-                        <div>{res.original_title}</div>
-                      </div>
-                    ))}
+                  {indexResults.map(res => (
+                    <div className="suggestion">
+                      <div>{res.original_title}</div>
+                    </div>
+                  ))}
                 </div>
               </div>
               <div className="resultCategory list">
@@ -41,17 +59,13 @@ const App = () => {
               <div className="resultPopular list">
                 <div className="listHead">Popular in "{value}"</div>
                 <div className="listBody">
-                  {data
-                    .filter(
-                      res =>
-                        res._suggestion_type === 'popular' &&
-                        res.indices.includes('movies-store-app')
-                    )
-                    .map(res => (
-                      <div className="suggestion">
-                        <div>{res.value}</div>
-                      </div>
-                    ))}
+                  {popularResults.length
+                    ? popularResults.map(res => (
+                        <div className="suggestion">
+                          <div>{res.value}</div>
+                        </div>
+                      ))
+                    : 'No Results'}
                 </div>
               </div>
             </div>
